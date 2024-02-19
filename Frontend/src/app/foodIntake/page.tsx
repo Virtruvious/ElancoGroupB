b6487bb564b8254a5d9daf91dc0979196fcfacc3
Flinn Alexnader Hancock <flinnh11@gmail.com>
2024-02-19 10:35:54 +0000

    Adds Graph Functionality and adds new pages
    title

---
+++ Frontend/src/app/foodIntake/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex flex-row min-h-screen">
+      <div className="bg-white w-full text-black">Put information about the dog's food consumption in calories here, maybe flag to user if dog isn't eating much</div>
+    </main>
+  );
+}
---
+++ Frontend/src/app/waterIntake/page.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex flex-row min-h-screen">
+      <div className="bg-white w-full text-black">Put information about the dog's water consumption in ml here, maybe flag to user if dog isn't eating much</div>
+    </main>
+  );
+}
