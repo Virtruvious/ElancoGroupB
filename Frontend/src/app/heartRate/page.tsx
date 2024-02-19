5b23c26ee296e00a03d4bfe97a2b7a4448ad683e
Flinn Alexnader Hancock <flinnh11@gmail.com>
2024-02-19 13:47:17 +0000

    Modify line graphs
    Change the line graphs to only use one line

--- Frontend/src/app/heartRate/page.tsx
+++ Frontend/src/app/heartRate/page.tsx
@@ -1,7 +1,78 @@
-export default function Home() {
-  return (
-    <main className="flex flex-row min-h-screen">
-      <div className="bg-white w-full text-black">Put information about the dog's heart rate here and potentially flag any abnormalities</div>
-    </main>
-  );
-}
+'use client'
+import "./styles.css";
+
+
+
+import React, { Component } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+
+
+export default function App() {
+    const data = [
+        {
+          name: "Page A",
+          uv: 4000,
+        },
+        {
+          name: "Page B",
+          uv: 3000,
+        },
+        {
+          name: "Page C",
+          uv: 2000,
+        },
+        {
+          name: "Page D",
+          uv: 2780,
+        },
+        {
+          name: "Page E",
+          uv: 1890,
+        },
+        {
+          name: "Page F",
+          uv: 2390,
+        },
+        {
+          name: "Page G",
+          uv: 3490,
+        }
+      ];
+
+    return (
+        <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
+    );
+  }
+  
