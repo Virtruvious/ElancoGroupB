131adfaa9a5fba83ef082c5d8d2fdd8d31b29936
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2024-02-22 13:12:42 +0000

    adjustments for new design (remaining pages)

--- Frontend/src/app/foodIntake/page.tsx
+++ Frontend/src/app/foodIntake/page.tsx
@@ -4,9 +4,9 @@ import BarChartGraph from "@/components/charts/foodIntake";
 
 export default function Home() {
   return (
-    <main className="flex flex-row min-h-screen">
+    <main className="min-h-screen">
       <Sidebar />
-      <div className="bg-white w-full text-black h-screen overflow-auto">
+      <div className="bg-white text-black w-[cal(100%-72px)] md:w-[cal(100%-244px)] h-screen overflow-auto ml-[72px] md:ml-[244px]">
       <div className="absolute top-0 right-0 text-elanco p-10">
           <a href="..">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 cursor-pointer">
