74aa738420911b9fd1b6ec89c67c82a740f0d5e8
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2024-02-22 09:23:54 +0000

    changed collapse screen size

--- Frontend/src/components/sidebar.tsx
+++ Frontend/src/components/sidebar.tsx
@@ -22,11 +22,11 @@ export default function Sidebar() {
   ];
 
   return (
-    <aside className="group flex flex-col lg:p-1 bg-elanco">
+    <aside className="group flex flex-col md:p-1 bg-elanco">
       <img
         src="/LogoWhite.png"
         alt="Elanco Logo"
-        className="p-3 lg:p-5 w-[5rem] lg:w-[16rem] group-hover:w-[16rem] duration-1000"
+        className="p-3 md:p-5 w-[5rem] md:w-[16rem] group-hover:w-[16rem] duration-1000"
 
       />
       <nav className="flex flex-col gap-y-3 items-center mx-3">
@@ -34,13 +34,13 @@ export default function Sidebar() {
           <a
             key={item.name}
             href={item.href}
-            className="flex lg:gap-3 group-hover:gap-3 p-3 w-full rounded-lg text-white hover:text-elanco hover:bg-white outline-white duration-200"
+            className="flex md:gap-3 group-hover:gap-3 p-3 w-full rounded-lg text-white hover:text-elanco hover:bg-white outline-white duration-200"
           >
             {item.icon}
 
             <span className="
-                w-0 lg:w-auto group-hover:w-auto whitespace-nowrap
-                scale-x-0 lg:scale-x-100 group-hover:scale-x-100
+                w-0 md:w-auto group-hover:w-auto whitespace-nowrap
+                scale-x-0 md:scale-x-100 group-hover:scale-x-100
                 origin-left transition-transform duration-1000">{item.name}</span>
           </a>
         ))}
@@ -52,15 +52,15 @@ export default function Sidebar() {
         
         <button
           onClick={() => signOut()}
-          className="flex justify-center lg:gap-3 group-hover:gap-3 p-3 
+          className="flex justify-center md:gap-3 group-hover:gap-3 p-3 
                 w-full rounded-lg bg-white font-light text-black outline-elanco
                 hover:bg-opacity-70 duration-200">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
           </svg>
 
-          <span className=" w-0 lg:w-auto group-hover:w-auto whitespace-nowrap
-                scale-x-0 lg:scale-x-100 group-hover:scale-x-100
+          <span className=" w-0 md:w-auto group-hover:w-auto whitespace-nowrap
+                scale-x-0 md:scale-x-100 group-hover:scale-x-100
                 origin-left transition-transform duration-1000">Logout</span>
         </button>
         
