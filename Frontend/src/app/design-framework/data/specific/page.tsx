f43de3089146e00734faf8d77ba5671e5bba9f2b
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2024-02-22 10:52:26 +0000

    added back button in design framework

--- Frontend/src/app/design-framework/data/specific/page.tsx
+++ Frontend/src/app/design-framework/data/specific/page.tsx
@@ -1,12 +1,17 @@
 import Sidebar from "@/components/sidebar";
 
 export default function Home() {
-    
   return (
     <main className="flex flex-row min-h-screen">
       <Sidebar />
       <div className="bg-white w-full text-black h-screen overflow-auto">
-
+        <div className="absolute top-0 right-0 text-elanco p-10">
+          <a href="../../../dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 cursor-pointer">
+              <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="3" d="M20 20L4 4m16 0L4 20" />
+            </svg>
+          </a>
+        </div>
         <div className="p-10 grid lg:grid-cols-6 gap-3">
             <div className="lg:col-span-2 text-center lg:text-start">
                 <div className="font-bold text-elanco text-xl md:text-3xl xl:text-5xl">Heart Rate</div>
