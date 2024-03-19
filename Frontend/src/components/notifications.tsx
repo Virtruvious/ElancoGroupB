1cb88db5d4e728982b5051154dc9673f180d452c
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2024-03-19 10:52:10 +0000

    fixed syntax

--- Frontend/src/components/notifications.tsx
+++ Frontend/src/components/notifications.tsx
@@ -44,12 +44,12 @@ export default function Notification({side}:{side:boolean}) {
             Notification
             <div className={`${side ? "hidden": "absolute"} top-2 right-2 cursor-pointer`} id="hideNoti">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="size-8 md:size-10">
-                    <path fill="currentColor" fill-rule="evenodd" stroke="currentColor" stroke-linejoin="round" stroke-width="4" d="M44 40.836c-4.893-5.973-9.238-9.362-13.036-10.168c-3.797-.805-7.412-.927-10.846-.365V41L4 23.545L20.118 7v10.167c6.349.05 11.746 2.328 16.192 6.833c4.445 4.505 7.009 10.117 7.69 16.836Z" clip-rule="evenodd" />
+                    <path fill="currentColor" fillRule="evenodd" stroke="currentColor" strokeLinejoin="round" strokeWidth="4" d="M44 40.836c-4.893-5.973-9.238-9.362-13.036-10.168c-3.797-.805-7.412-.927-10.846-.365V41L4 23.545L20.118 7v10.167c6.349.05 11.746 2.328 16.192 6.833c4.445 4.505 7.009 10.117 7.69 16.836Z" clipRule="evenodd" />
                 </svg>
             </div>
         </div>
         {notifications.map((noti, index) => (
-        <div className={`border-y-2 p-3 ${noti.read ? "opacity-60": "cursor-pointer"}`} onClick={() => markRead(index)}>
+        <div key={index} className={`border-y-2 p-3 ${noti.read ? "opacity-60": "cursor-pointer"}`} onClick={() => markRead(index)}>
             <div className={`text-elanco ${side ? "text-3xl":"text-3xl lg:text-4xl"} font-semibold`}>{noti.title}</div>
             <div className="text-md">Here comes description.</div>
             <div><span className="text-elanco font-semibold">Date: </span>18.3.2024</div>
