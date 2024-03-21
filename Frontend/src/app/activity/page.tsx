63af12af30cf49048d4c5d85ff7cf5016f2af64e
Win3065213 <AungNaing.N.Win@student.shu.ac.uk>
2024-03-21 15:08:20 +0000

    Removed activity and breathing rate pages

--- Frontend/src/app/activity/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex flex-row min-h-screen">
-      <div className="bg-white w-full text-black">Step counter, how many steps a dog has taken each hour, though probably more useful to a user if compiled to a single day of data. Probably best to label this page "step counter" for the user or something rather than activity, which the csv uses</div>
-    </main>
-  );
-}
